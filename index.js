576796f9489ad23098e658d1dea056f22330b817
Brunoaparecido19 <ap24201@icloud.com>
2021-12-06 13:05:31 -0300

    Teste

--- index.js
+++ index.js
@@ -1,32 +1,123 @@
 const formOne = document.querySelector(".form-one");
 const formTwo = document.querySelector(".form-two");
+const formThree = document.querySelector(".form-three");
+const formFour = document.querySelector(".form-four");
+const formFive = document.querySelector(".form-five");
+const formSix = document.querySelector(".form-six");
+const formSeven = document.querySelector(".form-seven");
+const formEight = document.querySelector(".form-eight");
+const formNine = document.querySelector(".form-nine");
+const formTen = document.querySelector(".form-ten");
 const printData = document.querySelector(".print-data");
+
 
 function submitFormOne() {
   formOne.style.display = "none";
   formTwo.style.display = "block";
+  formThree.style.display = "none";
+  formFour.style.display = "none";
+  formFive.style.display = "none";
+  formSix.style.display = "none";
+  formSeven.style.display = "none";
+  formEight.style.display = "none";
+  formNine.style.display = "none";
+  formTen.style.display = "none";
 }
 function submitFormTwo() {
   formOne.style.display = "none";
   formTwo.style.display = "none";
+  formTwo.style.display = "none";
+  formThree.style.display = "block";
+  formFour.style.display = "none";
+  formFive.style.display = "none";
+  formSix.style.display = "none";
+  formSeven.style.display = "none";
+  formEight.style.display = "none";
+  formNine.style.display = "none";
+  formTen.style.display = "none";
+}
+function submitFormThree() {
+  formOne.style.display = "none";
+  formTwo.style.display = "none";
+  formThree.style.display = "none";
+  formFour.style.display = "block";
+  formFive.style.display = "none";
+  formSix.style.display = "none";
+  formSeven.style.display = "none";
+  formEight.style.display = "none";
+  formNine.style.display = "none";
+  formTen.style.display = "none";
+}
+function submitFormFour() {
+  formOne.style.display = "none";
+  formTwo.style.display = "none";
+  formThree.style.display = "none";
+  formFour.style.display = "none";
+  formFive.style.display = "block";
+  formSix.style.display = "none";
+  formSeven.style.display = "none";
+  formEight.style.display = "none";
+  formNine.style.display = "none";
+  formTen.style.display = "none";
+}
+function submitFormFive() {
+  formOne.style.display = "none";
+  formTwo.style.display = "none";
+  formThree.style.display = "none";
+  formFour.style.display = "none";
+  formFive.style.display = "none";
+  formSix.style.display = "block";
+  formSeven.style.display = "none";
+  formEight.style.display = "none";
+  formNine.style.display = "none";
+  formTen.style.display = "none";
+}
+function submitFormSix() {
+  formSix.style.display = "none";
+  formSeven.style.display = "block";
+}
+function submitFormSeven() {
+  formOne.style.display = "none";
+  formTwo.style.display = "none";
+  formThree.style.display = "none";
+  formFour.style.display = "none";
+  formFive.style.display = "none";
+  formSeven.style.display = "none";
+  formEight.style.display = "block";
+  formNine.style.display = "none";
+  formTen.style.display = "none";
+}
+function submitFormEight() {
+  formOne.style.display = "none";
+  formTwo.style.display = "none";
+  formThree.style.display = "none";
+  formFour.style.display = "none";
+  formFive.style.display = "none";
+  formSeven.style.display = "none";
+  formEight.style.display = "none";
+  formNine.style.display = "block";
+  formTen.style.display = "none";
+}
+function submitFormNine() {
+  formOne.style.display = "none";
+  formTwo.style.display = "none";
+  formThree.style.display = "none";
+  formFour.style.display = "none";
+  formFive.style.display = "none";
+  formSeven.style.display = "none";
+  formEight.style.display = "none";
+  formNine.style.display = "none";
+  formTen.style.display = "block";
+}
+function submitFormTen() {
+  formOne.style.display = "none";
+  formTwo.style.display = "none";
+  formThree.style.display = "none";
+  formFour.style.display = "none";
+  formFive.style.display = "none";
+  formSeven.style.display = "none";
+  formEight.style.display = "none";
+  formNine.style.display = "none";
+  formTen.style.display = "none";
   printData.style.display = "block";
-  saveFormData();
-}
-
-function saveFormData() {
-  const formOneData = {
-    name: formOne.querySelector("#Name").value,
-    email: formOne.querySelector("#E-mail").value,
-    phone: formOne.querySelector("#Phone").value,
-    address: formTwo.querySelector("#Address").value,
-    city: formTwo.querySelector("#City").value,
-    state: formTwo.querySelector("#State").value,
-    zip: formTwo.querySelector("#Zip").value,
-  };
-  localStorage.setItem("formOneData", JSON.stringify(formOneData));
-}
-
-function getFormData() {
-  const formOneData = JSON.parse(localStorage.getItem("formOneData"));
-  console.log(formOneData);
 }
